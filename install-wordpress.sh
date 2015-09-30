710811f70feae99aa723185e1dfdb78638774ffd
Jim Hamill <jim@optimising.com.au>
2015-09-30 21:59:41 +1000

    Updated scripts

--- install-wordpress.sh
+++ install-wordpress.sh
@@ -1,1 +1,3 @@
- 
+cd /var/www
+wget http://wordpress.org/latest.tar.gz
+tar -xzvf latest.tar.gz 
---
+++ mysql-add-db.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+  
+EXPECTED_ARGS=3
+E_BADARGS=65
+MYSQL=`which mysql`
+  
+Q1="CREATE DATABASE IF NOT EXISTS $2;"
+Q2="GRANT USAGE ON *.* TO $3@localhost IDENTIFIED BY '$4';"
+Q3="GRANT ALL PRIVILEGES ON $2.* TO $3@localhost;"
+Q4="FLUSH PRIVILEGES;"
+SQL="${Q1}${Q2}${Q3}${Q4}"
+  
+if [ $# -ne $EXPECTED_ARGS ]
+then
+  echo "Usage: $0 dbname dbuser dbpass"
+  exit $E_BADARGS
+fi
+  
+$MYSQL -u root -p$1 -e "$SQL"
